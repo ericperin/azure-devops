77f1675fc6af759e6ce0f2bab88bff0bfb051418
Eric Perin <eric.perin@outlook.com>
2019-12-15 11:09:45 -0400

    Added git id

--- src/Example.Web/Startup.cs
+++ src/Example.Web/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,7 +9,7 @@ namespace Example.Web
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {
+        {//git: 0ec92ae3373054377aca70364882bb18c6a9dd2b
             Configuration = configuration;
         }
 
