77b9b1403a8167979287ca6cdfb0440ce683ea11
Eric Perin <eric.perin@outlook.com>
2019-12-15 10:54:57 -0400

    Added api project

--- src/Example.Web/Pages/Index.cshtml.cs
+++ src/Example.Web/Pages/Index.cshtml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -11,15 +7,21 @@ namespace Example.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
         public void OnGet()
         {
+            _logger.LogInformation($"!!!! {_env.EnvironmentName} !!!");
 
+            _logger.LogError("LogError....");
+            _logger.LogInformation("LogInformation....");
+            _logger.LogWarning("LogWarning....");
         }
     }
 }
